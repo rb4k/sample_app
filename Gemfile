d0262f7f915328b705775e879a1d8d366b4cdc9e
rob4ik@yahoo.de <rob4ik@yahoo.de>
2014-12-02 21:56:03 +0100

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ group :development, :test do
   # gem 'childprocess', '0.3.6'
   # gem 'spork', '0.9.2'
 end
+group :development do
+  gem 'annotate', '2.5.0'
+end
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ SampleApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
-  # You can have the root of your site routed with "root"
+  # Ruby-Zauberrei... help_path Durch das _path weiß Ruby, dass wir hier die Pfad definiert haben...
+    
+# You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
 
